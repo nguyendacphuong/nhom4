9e088dc520ac6372e793f6e51429f740a1d87a3c
nguyendacphuong <phuongndph13724@fpt.edu.vn>
2021-12-03 21:31:57 +0700

    update

--- admin/business/donhang.php
+++ admin/business/donhang.php
@@ -1,6 +1,6 @@
 <?php 
 	function donhang(){
-		$sql = "select * from orders";
+		$sql = "select * from orders order by id desc";
     	$cates = executeQuery($sql, true);
 		admin_render('donhang/index.php', compact('cates'), 'admin-assets/custom/category_index.js');
 
