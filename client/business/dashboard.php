c269c9101f6b0eafb71d20c4eb831871f85fc03c
datntph13533 <93896178+datntph13533@users.noreply.github.com>
2021-12-09 10:57:20 +0700

    Update dashboard.php

--- client/business/dashboard.php
+++ client/business/dashboard.php
@@ -165,3 +165,20 @@ function favorite_profuct_remove()
     executeQuery($sql);
     header('Location: ' . $_SERVER['HTTP_REFERER']);
 }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
