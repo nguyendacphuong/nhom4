66eb636948d7520e3e90337f742ec6db99718970
datntph13533 <93896178+datntph13533@users.noreply.github.com>
2021-12-09 15:43:37 +0700

    Update doi_mk.php

--- client/views/homepage/doi_mk.php
+++ client/views/homepage/doi_mk.php
@@ -1,9 +1,8 @@
-
 <div style="margin-top: 50px;" class="my-account-area">
     <div class="container">
         <div class="row">
             <div class="col-md-6">
-                <form action="<?= BASE_URL . 'doi_mk'?>" method="POST">
+                <form action="<?= BASE_URL . 'doi_mk' ?>" method="POST">
                     <div class="form-fields">
                         <h2>XÁC NHẬN TÀI KHOẢN</h2>
                         <h2><?php if (isset($error) && ($error != "")) echo $error; ?></h2>
@@ -22,7 +21,7 @@
                 </form>
             </div>
             <div class="col-md-6">
-                <img class="" src="<?= IMAGE_URL .'dk1.jpg'?>">
+                <img class="" src="<?= IMAGE_URL . 'dk1.jpg' ?>">
             </div>
         </div>
     </div>
