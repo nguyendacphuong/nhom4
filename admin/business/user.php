b9fa590b040b5e5d9ee56fe852e7a54aa706e85e
datntph13533 <93896178+datntph13533@users.noreply.github.com>
2021-12-11 19:12:08 +0700

    Update user.php

--- admin/business/user.php
+++ admin/business/user.php
@@ -4,7 +4,6 @@ function user_index()
 	$keyword = isset($_GET['keyword']) ? $_GET['keyword'] : "";
 	$sql = "select * from user where email like '%$keyword%'";
 	$cates = executeQuery($sql, true);
-
 	admin_render('user/index.php', compact('cates', 'keyword'), 'admin-assets/custom/category_index.js');
 }
 function user_add()
