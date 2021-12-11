16f635a6e238248e2c97948472740801b740c90b
datntph13533 <93896178+datntph13533@users.noreply.github.com>
2021-12-11 22:33:07 +0700

    Update sidebar.php

--- admin/views/layouts/sidebar.php
+++ admin/views/layouts/sidebar.php
@@ -1,8 +1,8 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
   <!-- Brand Logo -->
-  <a href="index3.html" class="brand-link">
+  <a href="<?= BASE_URL ?>" class="brand-link">
     <img src="<?= ADMIN_ASSET ?>dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-    <span class="brand-text font-weight-light">AdminLTE 3</span>
+    <span class="brand-text font-weight-light">Trang chủ</span>
   </a>
 
   <!-- Sidebar -->
@@ -13,7 +13,7 @@
         <img src="<?= ADMIN_ASSET ?>dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
       </div>
       <div class="info">
-        <a href="#" class="d-block">Alexander Pierce</a>
+        <a href="#" class="d-block">ADMIN</a>
       </div>
     </div>
 
@@ -65,6 +65,31 @@
               </a>
             </li>
           </ul>
+        </li>
+
+        <li class="nav-item">
+          <a href="#" class="nav-link">
+            <i class="fas fa-registered" aria-hidden="true"></i>
+            <p>
+              Thương hiệu
+              <i class="fas fa-angle-left right"></i>
+            </p>
+          </a>
+          <ul class="nav nav-treeview">
+            <li class="nav-item">
+              <a href="<?= ADMIN_URL . 'thuong-hieu' ?>" class="nav-link">
+                <i class="far fa-circle nav-icon"></i>
+                <p>Danh sách thương hiệu</p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="<?= ADMIN_URL . 'thuong-hieu/tao-moi' ?>" class="nav-link">
+                <i class="far fa-circle nav-icon"></i>
+                <p>Tạo mới</p>
+              </a>
+            </li>
+          </ul>
+
         </li>
 
         <li class="nav-item">
@@ -139,32 +164,7 @@
           </ul>
         </li>
 
-        <li class="nav-item">
-          <a href="#" class="nav-link">
-            <i class="fas fa-registered" aria-hidden="true"></i>
-            <p>
-              Thương hiệu
-              <i class="fas fa-angle-left right"></i>
-            </p>
-          </a>
-          <ul class="nav nav-treeview">
-            <li class="nav-item">
-              <a href="<?= ADMIN_URL . 'thuong-hieu' ?>" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
-                <p>Danh sách thương hiệu</p>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a href="<?= ADMIN_URL . 'thuong-hieu/tao-moi' ?>" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
-                <p>Tạo mới</p>
-              </a>
-            </li>
-          </ul>
-
-        </li>
-
-        <li class="nav-item">
+        <!-- <li class="nav-item">
           <a href="#" class="nav-link">
             <i class="fas fa-users" aria-hidden="true"></i>
             <p>
@@ -179,10 +179,8 @@
                 <p>Thông tin liên hệ</p>
               </a>
             </li>
-
           </ul>
-
-        </li>
+        </li> -->
 
         <li class="nav-item">
           <a href="#" class="nav-link">
